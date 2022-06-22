a19e869299e70a1546a464713fe6f1d487bf42fd
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-22 14:20:30 -0500

    Ejercicio Spinner Resuelto

--- app/src/main/java/com/luismorales17106494/a006_controlspinner/MainActivity.kt
+++ app/src/main/java/com/luismorales17106494/a006_controlspinner/MainActivity.kt
@@ -2,6 +2,9 @@ package com.luismorales17106494.a006_controlspinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.luismorales17106494.a006_controlspinner.databinding.ActivityMainBinding
 
 /*
     6 - Control Spinner
@@ -20,13 +23,51 @@ mediante un control de tipo Button efectuamos la operación respectiva. Mostramo
 en un TextView.
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val lista = arrayOf("Sumar", "Restar", "Multiplicar", "Dividir")
+
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
+        binding.spOperar.adapter = adaptador
 
 
 
 
+        binding.btOperar.setOnClickListener() {
+
+            if (binding.etNumero1.text.isNotBlank() && binding.etNumero2.text.isNotBlank()) {
+
+                when (binding.spOperar.selectedItem.toString()) {
+                    "Sumar" -> {
+                        val sumar = binding.etNumero1.text.toString()
+                            .toDouble() + binding.etNumero2.text.toString().toDouble()
+                        binding.tvResultado.text = sumar.toString()
+                    }
+                    "Restar" -> {
+                        val resta = binding.etNumero1.text.toString()
+                            .toDouble() - binding.etNumero2.text.toString().toDouble()
+                        binding.tvResultado.text = resta.toString()
+                    }
+                    "Multiplicar" -> {
+                        val multi = binding.etNumero1.text.toString()
+                            .toDouble() * binding.etNumero2.text.toString().toDouble()
+                        binding.tvResultado.text = multi.toString()
+                    }
+                    "Dividir" -> {
+                        val dividir = binding.etNumero1.text.toString()
+                            .toDouble() / binding.etNumero2.text.toString().toDouble()
+                        binding.tvResultado.text = dividir.toString()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Rellene todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
