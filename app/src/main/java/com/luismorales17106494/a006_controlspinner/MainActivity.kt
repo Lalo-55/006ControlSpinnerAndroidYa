eaf6c2c3876599b17ff993668ec1c03dc4182994
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-22 13:41:46 -0500

    Ejercicio Spinner sin resolver

---
+++ app/src/main/java/com/luismorales17106494/a006_controlspinner/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.luismorales17106494.a006_controlspinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+    6 - Control Spinner
+El objetivo de este concepto es seguir practicando lo visto hasta ahora e incorporar el
+control visual Spinner.
+El control Spinner muestra una lista de String y nos permite seleccionar uno de ellos. Cuando
+se lo selecciona se abre y muestra todos sus elementos para permitir seleccionar uno de ellos.
+
+
+  Problema:
+Realizar la carga de dos números en controles de tipo EditText ("Number").
+Mostrar un mensaje que solicite la carga de los valores iniciando la
+propiedad "hint" de cada control. Disponer un control de tipo Spinner que permita
+seleccionar si queremos sumar, restar, multiplicar o dividir dichos valores. Finalmente
+mediante un control de tipo Button efectuamos la operación respectiva. Mostramos el resultado
+en un TextView.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
